7bf63bea1f6c696ab0dc7b438fc1e96617524af8
martyd420 <git@pcdr.cz>
2024-12-08 17:00:39 +0100

    added user presenter

---
+++ private/app/AdminModule/Presenters/UserPresenter.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\AdminModule\Presenters;
+
+use App\AdminModule\Presenters\BaseAdminPresenter;
+
+final class UserPresenter extends BaseAdminPresenter
+{
+
+	public function renderDefault()
+	{
+
+	}
+
+}
