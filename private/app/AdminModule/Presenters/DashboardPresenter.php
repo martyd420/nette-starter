597657d05748f7bdec14497dfe85b63040a487bb
martyd420 <git@pcdr.cz>
2024-12-08 15:51:19 +0100

    added adminlte

---
+++ private/app/AdminModule/Presenters/DashboardPresenter.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\AdminModule\Presenters;
+
+use App\AdminModule\Presenters\BaseAdminPresenter;
+
+final class DashboardPresenter extends BaseAdminPresenter
+{
+
+	public function renderDefault()
+	{
+
+	}
+
+}
