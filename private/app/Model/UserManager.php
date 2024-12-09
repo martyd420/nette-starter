804a3dcd9e7ed0f6de8719831052e52a75a99e22
martyd420 <git@pcdr.cz>
2024-12-09 15:16:59 +0100

    added function to update user

--- private/app/Model/UserManager.php
+++ private/app/Model/UserManager.php
@@ -4,6 +4,7 @@ namespace App\Model;
 
 use App\Model\Entity\User;
 use Nette\Security\Passwords;
+use Nette\Utils\ArrayHash;
 use Nettrine\ORM\Decorator\SimpleEntityManagerDecorator;
 
 class UserManager
@@ -18,7 +19,22 @@ class UserManager
 		$this->passwords = $passwords;
 	}
 
+	public function updateUser(int $id, ArrayHash $values): User
+	{
+		$user = $this->em->getRepository(User::class)->find($id);
+		if(!$user) {
+			throw new \Exception('User not found');
+		}
+		$user->setEmail($values->email);
+		$user->setNick($values->nick);
+		$user->setActive((int)$values->active);
 
+		$this->em->persist($user);
+		$this->em->flush();
+
+		return $user;
+	}
+	
 	public function createUser($email, $password, $nick = null): void
 	{
 		$user = new User();
