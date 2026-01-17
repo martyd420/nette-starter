e8146276bfdda29251776be557a3ec4678af5c61
martyd420 <github-public@pcdr.cz>
2026-01-17 04:11:16 +0100

    added nette forms

--- private/assets/js/front/front.js
+++ private/assets/js/front/front.js
@@ -1,7 +1,8 @@
 import naja from 'naja';
 import '@/css/front/style.css';
+import netteForms from 'nette-forms';
 
 document.addEventListener('DOMContentLoaded', () => {
     naja.initialize({ history: false });
-    console.log('Frontend initialized with Naja');
+    netteForms.initOnLoad();
 });
